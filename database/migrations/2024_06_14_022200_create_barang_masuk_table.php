ee9b15f8453114c41a228152ffac7d1f5dcbe9d0
root <root@debian12.top>
2024-06-14 09:25:34 +0700

    membuat migration table dan migration trigger

---
+++ database/migrations/2024_06_14_022200_create_barang_masuk_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangmasuk', function (Blueprint $table) {
+            $table->id();
+            $table->date('tgl_masuk');
+            $table->smallInteger('qty_masuk')->default(1);
+            $table->foreignId('barang_id')->constrained('barang')->cascadeOnUpdate()->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangmasuk');
+    }
+};
